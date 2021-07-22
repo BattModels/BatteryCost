2924fe148ded581cd05dc9156ea4bfc36bee7665
Alec Bills <abills@andrew.cmu.edu>
2021-07-22 16:51:57 -0500

    Add BatteryCost prefix to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using BatteryCost
 using Test
 cell1 = cell()
-cost1 = cost_default()
-cell3,cost3 = convert_all(cell1,cost1,mult)
+cost1 = BatteryCost.cost_default()
+cell3,cost3 = BatteryCost.convert_all(cell1,cost1,mult)
 @test cost_calc(cell3,cost3)==(209.68524992517177, 1708.1761931921092)
