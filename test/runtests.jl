e0b6d170ba7607d75a33016a178434e926bd64ea
Alec Bills <abills@andrew.cmu.edu>
2021-07-22 16:58:26 -0500

    add testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using BatteryCost
 using Test
-cell1 = cell()
-cost1 = BatteryCost.cost_default()
-cell3,cost3 = BatteryCost.convert_all(cell1,cost1,BatteryCost.mult)
-@test cost_calc(cell3,cost3)==(209.68524992517177, 1708.1761931921092)
+
+@testset "cost calculation" begin
+    cell1 = cell()
+    cost1 = BatteryCost.cost_default()
+    cell3,cost3 = BatteryCost.convert_all(cell1,cost1,BatteryCost.mult)
+    @test cost_calc(cell3,cost3)==(209.68524992517177, 1708.1761931921092)
+end
+
